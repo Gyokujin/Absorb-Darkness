fd20c9f98427c5674da088a4c37a839d1c1aff56
Gyokujin <dd43454548@naver.com>
2024-02-13 10:56:47 +0900

    # Player Input 수정

--- Assets/Scripts/Player/InputHandler.cs
+++ Assets/Scripts/Player/InputHandler.cs
@@ -2,50 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace PlayerInput
+public class InputHandler : MonoBehaviour
 {
-    public class InputHandler : MonoBehaviour
+    public float horizontal;
+    public float vertical;
+    public float moveAmount;
+    public float mouseX;
+    public float mouseY;
+
+    private PlayerControls inputActions;
+
+    private Vector2 movementInput;
+    private Vector2 cameraInput;
+
+    public void OnEnable()
     {
-        public float horizontal;
-        public float vertical;
-        public float moveAmount;
-        public float mouseX;
-        public float mouseY;
-
-        PlayerControls inputActions;
-
-        Vector2 movementInput;
-        Vector2 cameraInput;
-
-        public void OnEnable()
+        if (inputActions == null)
         {
-            if (inputActions == null)
-            {
-                inputActions = new PlayerControls();
-                inputActions.PlayerMovement.Movement.performed += inputActions => movementInput = inputActions.ReadValue<Vector2>();
-                inputActions.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
-            }
-
-            inputActions.Enable();
+            inputActions = new PlayerControls();
+            inputActions.PlayerMovement.Movement.performed += inputActions => movementInput = inputActions.ReadValue<Vector2>();
+            inputActions.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
         }
 
-        private void OnDisable()
-        {
-            inputActions.Disable();
-        }
+        inputActions.Enable();
+    }
 
-        public void TickInput(float delta)
-        {
-            MoveInput(delta);
-        }
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
 
-        private void MoveInput(float delta)
-        {
-            horizontal = movementInput.x;
-            vertical = movementInput.y;
-            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
-            mouseX = cameraInput.x;
-            mouseY = cameraInput.y;
-        }
+    public void TickInput(float delta)
+    {
+        MoveInput(delta);
+    }
+
+    private void MoveInput(float delta)
+    {
+        horizontal = movementInput.x;
+        vertical = movementInput.y;
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+        mouseX = cameraInput.x;
+        mouseY = cameraInput.y;
     }
 }
--- Assets/Scripts/Player/PlayerLocomotion.cs
+++ Assets/Scripts/Player/PlayerLocomotion.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class PlayerLocomotion : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform cameraObject;
+    private InputHandler inputHandler;
+    private Vector3 moveDirection;
+
+    [HideInInspector]
+    public Transform playerTransform;
+
+    public new Rigidbody rigidbody;
+    public GameObject normalCamera;
+
+    [Header("Status")]
+    [SerializeField]
+    private float movementSpeed = 5;
+    [SerializeField]
+    private float rotationSpeed = 10;
+
     void Start()
     {
-        
+        Init();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Init()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        inputHandler = GetComponent<InputHandler>();
+        cameraObject = Camera.main.transform;
+        playerTransform = transform;
     }
+
+    public void Update()
+    {
+        float delta = Time.deltaTime;
+
+        inputHandler.TickInput(delta);
+
+        moveDirection = cameraObject.forward * inputHandler.vertical;
+        moveDirection += cameraObject.right * inputHandler.horizontal;
+        moveDirection.Normalize();
+
+        float speed = movementSpeed;
+        moveDirection *= speed;
+
+        Vector3 projectVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+        rigidbody.velocity = projectVelocity;
+    }
+
+    #region Movement
+    private Vector3 normalVector;
+    private Vector3 targetPosition;
+
+    private void HandleRotation(float delta)
+    {
+        Vector3 targetDir = Vector3.zero;
+        float moveOverride = inputHandler.moveAmount;
+
+        targetDir = cameraObject.forward * inputHandler.vertical;
+        targetDir += cameraObject.right * inputHandler.horizontal;
+
+        targetDir.Normalize();
+        targetDir.y = 0;
+
+        if (targetDir == Vector3.zero)
+            targetDir = playerTransform.forward;
+
+        float rs = rotationSpeed;
+
+        Quaternion tr = Quaternion.LookRotation(targetDir);
+        Quaternion targetRotation = Quaternion.Slerp(playerTransform.rotation, tr, rs * delta);
+
+        playerTransform.rotation = targetRotation;
+    }
+
+    #endregion
 }
