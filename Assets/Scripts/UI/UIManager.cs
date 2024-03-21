8d27f46589d3ab1f6677b9eb4b396ddfc1e09131
Gyokujin <dd43454548@naver.com>
2024-03-22 00:57:57 +0900

    # 설정 UI 수정

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public GameObject selectWindow;
+    
+    public void ControlSelectWindow(bool onOpen)
+    {
+        selectWindow.SetActive(onOpen);
+    }
+
+    //public void OpenSelectWindow()
+    //{
+    //    selectWindow.SetActive(true);
+    //}
+
+    //public void CloseSelectWindow()
+    //{
+    //    selectWindow.SetActive(false);
+    //}
 }
