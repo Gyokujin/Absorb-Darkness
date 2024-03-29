8ccd54850a3f505b70abb557ac9db64338fa23d1
R3M4 <dd43454548@naver.com>
2024-03-29 19:14:57 +0900

    # 플레이어 리소스 변경 및 스크립트 업데이트

---
+++ Assets/Scripts/Player/CharacterManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform lockOnTramsform;
+}
