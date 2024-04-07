a1edd1932af004bc39a7770c80d0f19266c864e9
Gyokujin <dd43454548@naver.com>
2024-04-07 16:08:09 +0900

    # Enemy 공격 패턴 초기 구현

--- Assets/Scripts/Character/Enemy/EnemyManager.cs
+++ Assets/Scripts/Character/Enemy/EnemyManager.cs
@@ -10,16 +10,30 @@ public class EnemyManager : CharacterManager
     }
 
     [Header("State")]
-    public EnemyState state;
+    public EnemyState state = EnemyState.Idle;
     public bool isPreformingAction;
+
+    [Header("Attack")]
+    [SerializeField]
+    private EnemyAttackAction[] enemyAttacks;
+    public EnemyAttackAction currentAttack;
+    public float currentRecoveryTime = 0;
 
     [Header("Component")]
     private EnemyMove enemyMove;
+    private EnemyStats enemyStats;
+    private EnemyAnimator enemyAnimator;
 
     void Awake()
     {
-        state = EnemyState.Idle;
         enemyMove = GetComponent<EnemyMove>();
+        enemyStats = GetComponent<EnemyStats>();
+        enemyAnimator = GetComponentInChildren<EnemyAnimator>();
+    }
+
+    void Update()
+    {
+        HandleRecoveryTimer();
     }
 
     void FixedUpdate()
@@ -33,9 +47,83 @@ public class EnemyManager : CharacterManager
         {
             enemyMove.HandleDetection();
         }
-        else
+        else if (enemyMove.targetDistance > enemyMove.stopDistance)
         {
             enemyMove.HandleMoveTarget();
+        }
+        else if (enemyMove.targetDistance <= enemyMove.stopDistance)
+        {
+
+        }
+    }
+
+    void AttackTarget()
+    {
+        if (isPreformingAction)
+            return;
+
+        if (currentAttack == null)
+        {
+            GetNewAttack();
+        }
+        else
+        {
+            isPreformingAction = true;
+            enemyAnimator.PlayTargetAnimation(currentAttack.actionAnimation, true);
+        }
+    }
+
+    void GetNewAttack()
+    {
+        Vector3 targetDir = enemyMove.currentTarget.transform.position - transform.position;
+        float viewableAngle = Vector3.Angle(targetDir, transform.forward);
+        enemyMove.targetDistance = Vector3.Distance(enemyMove.currentTarget.transform.position, transform.position);
+
+        int maxScore = 0;
+
+        for (int i = 0; i < enemyAttacks.Length; i++)
+        {
+            EnemyAttackAction enemyAttackAction = enemyAttacks[i];
+
+            if (enemyMove.targetDistance <= enemyAttackAction.attackDisMax && enemyMove.targetDistance >= enemyAttackAction.attackDisMin
+                && viewableAngle <= enemyAttackAction.attackAngleMax && viewableAngle >= enemyAttackAction.attackAngleMin)
+            {
+                maxScore += enemyAttackAction.attackScore;
+            }
+        }
+
+        int randomValue = Random.Range(0, maxScore);
+        int tempScore = 0;
+
+        for (int i = 0; i < enemyAttacks.Length; i++)
+        {
+            EnemyAttackAction enemyAttackAction = enemyAttacks[i];
+
+            if (enemyMove.targetDistance <= enemyAttackAction.attackDisMax && enemyMove.targetDistance >= enemyAttackAction.attackDisMin
+                && viewableAngle <= enemyAttackAction.attackAngleMax && viewableAngle >= enemyAttackAction.attackAngleMin)
+            {
+                if (currentAttack != null)
+                    return;
+
+                tempScore += enemyAttackAction.attackScore;
+
+                if (tempScore > randomValue)
+                {
+                    currentAttack = enemyAttackAction;
+                }
+            }
+        }
+    }
+
+    void HandleRecoveryTimer()
+    {
+        if (currentRecoveryTime > 0)
+        {
+            currentRecoveryTime -= Time.deltaTime;
+        }
+        else if (isPreformingAction)
+        {
+            isPreformingAction = false;
         }
     }
 }
