25546300636b984134dad51cd3a7f37c517dd665
Gyokujin <dd43454548@naver.com>
2024-04-09 01:35:15 +0900

    # State 스크립트 생성으로 인한 리팩토링

--- Assets/Scripts/Character/Enemy/EnemyManager.cs
+++ Assets/Scripts/Character/Enemy/EnemyManager.cs
@@ -1,27 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyManager : CharacterManager
 {
     [Header("State")]
     public bool isPreformingAction;
 
+    [Header("PursueTarget")]
+    public float targetDistance;
+    public float stopDistance = 2f;
+
     [Header("Attack")]
     public float currentRecoveryTime = 0;
 
     [Header("Component")]
+    public NavMeshAgent navMeshAgent;
+    public new Rigidbody rigidbody;
     public CharacterStatus currentTarget;
     public EnemyState curState;
-    private EnemyMove enemyMove;
+    public EnemyStatus enemyStatus;
+    // private EnemyMove enemyMove;
     private EnemyAnimator enemyAnimator;
-    EnemyStatus enemyStatus;
 
     void Awake()
     {
-        enemyMove = GetComponent<EnemyMove>();
+        rigidbody = GetComponent<Rigidbody>();
+        navMeshAgent = GetComponentInChildren<NavMeshAgent>();
+        // enemyMove = GetComponent<EnemyMove>();
         enemyStatus = GetComponent<EnemyStatus>();
         enemyAnimator = GetComponentInChildren<EnemyAnimator>();
+    }
+
+    void Start()
+    {
+        rigidbody.isKinematic = false;
+        navMeshAgent.enabled = false;
+        navMeshAgent.stoppingDistance = stopDistance;
     }
 
     void Update()
@@ -50,71 +66,6 @@ public class EnemyManager : CharacterManager
     void SwitchNextState(EnemyState state)
     {
         curState = state;
-    }
-
-    void SelectAttackAction()
-    {
-        //if (isPreformingAction)
-        //    return;
-
-        //if (currentAttack == null)
-        //{
-        //    GetNewAttack();
-        //}
-        //else
-        //{
-        //    Attack();
-        //}
-    }
-
-    void GetNewAttack()
-    {
-        //Vector3 targetDir = enemyMove.currentTarget.transform.position - transform.position;
-        //float viewableAngle = Vector3.Angle(targetDir, transform.forward);
-        //enemyMove.targetDistance = Vector3.Distance(enemyMove.currentTarget.transform.position, transform.position);
-
-        //int maxScore = 0;
-
-        //for (int i = 0; i < enemyAttacks.Length; i++)
-        //{
-        //    EnemyAttackAction enemyAttackAction = enemyAttacks[i];
-
-        //    if (enemyMove.targetDistance <= enemyAttackAction.attackDisMax && enemyMove.targetDistance >= enemyAttackAction.attackDisMin &&
-        //        viewableAngle <= enemyAttackAction.attackAngleMax && viewableAngle >= enemyAttackAction.attackAngleMin)
-        //    {
-        //        maxScore += enemyAttackAction.attackScore;
-        //    }
-        //}
-
-        //int randomValue = Random.Range(0, maxScore);
-        //int tempScore = 0;
-
-        //for (int i = 0; i < enemyAttacks.Length; i++)
-        //{
-        //    EnemyAttackAction enemyAttackAction = enemyAttacks[i];
-
-        //    if (enemyMove.targetDistance <= enemyAttackAction.attackDisMax && enemyMove.targetDistance >= enemyAttackAction.attackDisMin &&
-        //        viewableAngle <= enemyAttackAction.attackAngleMax && viewableAngle >= enemyAttackAction.attackAngleMin)
-        //    {
-        //        if (currentAttack != null)
-        //            return;
-
-        //        tempScore += enemyAttackAction.attackScore;
-
-        //        if (tempScore > randomValue)
-        //        {
-        //            currentAttack = enemyAttackAction;
-        //        }
-        //    }
-        //}
-    }
-
-    void Attack()
-    {
-        //isPreformingAction = true;
-        //currentRecoveryTime = currentAttack.recoveryTime;
-        //enemyAnimator.PlayTargetAnimation(currentAttack.actionAnimation, true);
-        //currentAttack = null;
     }
 
     void HandleRecoveryTimer()
--- Assets/Scripts/Character/Enemy/EnemyMove.cs
+++ Assets/Scripts/Character/Enemy/EnemyMove.cs
@@ -1,101 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-using static EnemyManager;
 
 public class EnemyMove : MonoBehaviour
 {
-    [Header("Patrol")]
-    // public LayerMask detectionLayer;
-    public float targetDistance;
-    public float stopDistance = 2f;
-
-    [Header("Component")]
-    public new Rigidbody rigidbody;
-    private NavMeshAgent navMeshAgent;
-    private EnemyManager enemyManager;
-    private EnemyAnimator enemyAnimator;
-    private EnemyStatus enemyStatus;
+    // [Header("Component")]
+    // public new Rigidbody rigidbody;
+    // private EnemyManager enemyManager;
+    // private EnemyStatus enemyStatus;
 
     void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
-        navMeshAgent = GetComponentInChildren<NavMeshAgent>();
-        enemyManager = GetComponent<EnemyManager>();
-        enemyAnimator = GetComponentInChildren<EnemyAnimator>();
-        enemyStatus = GetComponent<EnemyStatus>();
-        navMeshAgent.stoppingDistance = stopDistance;
+        // rigidbody = GetComponent<Rigidbody>();
+        // enemyManager = GetComponent<EnemyManager>();
+        // enemyStatus = GetComponent<EnemyStatus>();
     }
 
     void Start()
     {
-        navMeshAgent.enabled = false;
-        rigidbody.isKinematic = false;
-    }
-
-    public void HandleMoveTarget()
-    {
-        if (enemyManager.isPreformingAction && targetDistance > stopDistance)
-            return;
-
-        Vector3 targetDirection = enemyManager.currentTarget.transform.position - transform.position;
-        targetDistance = Vector3.Distance(enemyManager.currentTarget.transform.position, transform.position);
-        float viewableAngle = Vector3.Angle(targetDirection, transform.forward);
-
-        if (enemyManager.isPreformingAction)
-        {
-            enemyAnimator.animator.SetFloat("vertical", 0, 0.1f, Time.deltaTime);
-            navMeshAgent.enabled = false;
-        }
-        else
-        {
-            if (targetDistance > stopDistance)
-            {
-                enemyAnimator.animator.SetFloat("vertical", 1, 0.1f, Time.deltaTime);
-                targetDirection.Normalize();
-                targetDirection.y = 0;
-                targetDirection *= enemyStatus.runSpeed;
-                Vector3 projectedVelocity = Vector3.ProjectOnPlane(targetDirection, Vector3.up);
-                rigidbody.velocity = projectedVelocity;
-            }
-            else if (targetDistance <= stopDistance)
-            {
-                enemyAnimator.animator.SetFloat("vertical", 0, 0.1f, Time.deltaTime);
-                rigidbody.velocity = Vector3.zero;
-            }
-        }
-
-        HandleRotateTarget();
-        navMeshAgent.transform.localPosition = Vector3.zero;
-        navMeshAgent.transform.localRotation = Quaternion.identity;
-    }
-
-    void HandleRotateTarget()
-    {
-        if (enemyManager.isPreformingAction)
-        {
-            Vector3 direction = enemyManager.currentTarget.transform.position - transform.position;
-            direction.y = 0;
-            direction.Normalize();
-
-            if (direction == Vector3.zero)
-            {
-                direction = Vector3.forward;
-            }
-
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, enemyStatus.rotationSpeed / Time.deltaTime);
-        }
-        else
-        {
-            Vector3 relativeDirection = transform.InverseTransformDirection(navMeshAgent.desiredVelocity);
-            Vector3 targetVelocity = rigidbody.velocity;
-
-            navMeshAgent.enabled = true;
-            navMeshAgent.SetDestination(enemyManager.currentTarget.transform.position);
-            navMeshAgent.velocity = targetVelocity;
-            transform.rotation = Quaternion.Slerp(transform.rotation, navMeshAgent.transform.rotation, enemyStatus.rotationSpeed / Time.deltaTime);
-        }
+        // rigidbody.isKinematic = false;
     }
 }
