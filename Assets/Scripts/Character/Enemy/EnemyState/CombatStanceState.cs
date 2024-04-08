e78e8b4e6108c1e23486df186fa304f838928967
Gyokujin <dd43454548@naver.com>
2024-04-09 01:34:33 +0900

    # CombatStanceState 스크립트 생성 및 스테이트 분류

--- Assets/Scripts/Character/Enemy/EnemyState/CombatStanceState.cs
+++ Assets/Scripts/Character/Enemy/EnemyState/CombatStanceState.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class CombatStanceState : EnemyState
 {
+    public AttackState attackState;
+    public PursueTargetState pursueTargetState;
+
     public override EnemyState Tick(EnemyManager enemyManager, EnemyStatus enemyStatus, EnemyAnimator enemyAnimator)
     {
-        return this;
+        enemyManager.targetDistance = Vector3.Distance(enemyManager.currentTarget.transform.position, enemyManager.transform.position);
+
+        if (enemyManager.currentRecoveryTime <= 0 && enemyManager.targetDistance <= enemyStatus.attackRangeMax)
+        {
+            return attackState;
+        }
+        else if (enemyManager.targetDistance > enemyStatus.attackRangeMax)
+        {
+            return pursueTargetState;
+        }
+        else
+        {
+            return this;
+        }
     }
 }
