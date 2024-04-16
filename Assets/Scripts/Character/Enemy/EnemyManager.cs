8f3a53d8349ac54720d17337014899608d8c02ce
Tirror <74170514+Gyokujin@users.noreply.github.com>
2024-04-16 16:27:27 +0900

    # EnemyManager 스크립트 리팩토링

--- Assets/Scripts/Character/Enemy/EnemyManager.cs
+++ Assets/Scripts/Character/Enemy/EnemyManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
 public class EnemyManager : CharacterManager
 {
     [Header("State")]
--- Assets/Scripts/Character/Enemy/EnemyMove.cs
+++ Assets/Scripts/Character/Enemy/EnemyMove.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider))]
 public class EnemyMove : MonoBehaviour
 {
     [Header("Component")]
     private EnemyManager enemyManager;
-
-    //[SerializeField]
-    //private Collider enemyCollider;
-    //[SerializeField]
-    //private Collider enemyBlockerCollider;
-    //private EnemyAnimator enemyAnimator;
 
     void Awake()
     {
@@ -22,7 +15,6 @@ public class EnemyMove : MonoBehaviour
     void Init()
     {
         enemyManager = GetComponent<EnemyManager>();
-        // enemyAnimator = GetComponentInChildren<EnemyAnimator>();
     }
 
     void Start()
