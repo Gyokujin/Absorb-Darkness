8e32aed476d5e3b2d61aad784edfebc31dbbf0f5
R3M4 <dd43454548@naver.com>
2024-04-17 20:45:49 +0900

    # PoolManager 추가

---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,56 @@
+using OpenCover.Framework.Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    public static PoolManager instance = null;
+
+    [Header("Spell")]
+    [SerializeField]
+    private GameObject[] spells;
+    private List<GameObject>[] spellPool;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        spellPool = new List<GameObject>[spells.Length];
+
+        for (int i = 0; i < spellPool.Length; i++)
+        {
+            spellPool[i] = new List<GameObject>();
+        }
+    }
+
+    public GameObject GetSpell(int index)
+    {
+        GameObject select = null;
+
+        foreach (GameObject spellObj in spellPool[0])
+        {
+            if (!spellObj.activeSelf)
+            {
+                select = spellObj;
+                select.SetActive(true);
+                break;
+            }
+        }
+
+        if (!select)
+        {
+            select = Instantiate(spells[index], transform);
+            spellPool[index].Add(select);
+        }
+
+        return select;
+    }
+}
