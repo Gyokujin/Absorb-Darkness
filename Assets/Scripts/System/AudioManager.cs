168071f096ed2313fc6a1649a64888c8b7a424bb
Gyokujin <dd43454548@naver.com>
2024-04-23 03:33:56 +0900

    # AudioManager 및 LightningImpact 스크립트 업데이트

---
+++ Assets/Scripts/System/AudioManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static PoolManager;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance = null;
+    public enum ActionSFX { Electric }
+
+    [Header("Action")]
+    public AudioClip[] actionClips;
+    [SerializeField]
+    [Range(0, 1)]
+    private float actionVolume;
+    [SerializeField]
+    private int actionCh;
+    private int actionIndex;
+    private AudioSource[] actionAudios;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        Init();
+    }
+
+    void Init()
+    {
+        GameObject actionAudio = new GameObject("ActionAudio");
+        actionAudio.transform.parent = transform;
+        actionAudios = new AudioSource[actionCh];
+
+        for (int k = 0; k < actionCh; k++)
+        {
+            actionAudios[k] = actionAudio.AddComponent<AudioSource>();
+            actionAudios[k].playOnAwake = false;
+            actionAudios[k].volume = actionVolume;
+        }
+    }
+
+    public void PlayActionSFX(AudioClip audioClip)
+    {
+        for (int i = 0; i < actionAudios.Length; i++)
+        {
+            int loopIndex = (i + actionIndex) % actionAudios.Length;
+
+            if (actionAudios[loopIndex].isPlaying)
+                continue;
+
+            actionIndex = loopIndex;
+            actionAudios[loopIndex].clip = audioClip;
+            actionAudios[loopIndex].Play();
+            break;
+        }
+    }
+}
