df3ce8877cc683a9a9e380ed6f848b7565099e69
Gyokujin <dd43454548@naver.com>
2024-04-23 01:40:14 +0900

    # 소서리스 사운드 재생 기능 추가

--- Assets/Scripts/Character/Enemy/Sorceress/Sorceress.cs
+++ Assets/Scripts/Character/Enemy/Sorceress/Sorceress.cs
@@ -41,6 +41,7 @@ public class Sorceress : MonoBehaviour
 
     [Header("Compnent")]
     private EnemyManager enemyManager;
+    private CharacterAudio characterAudio;
 
     void Awake()
     {
@@ -50,6 +51,7 @@ public class Sorceress : MonoBehaviour
     void Init()
     {
         enemyManager = GetComponent<EnemyManager>();
+        characterAudio = GetComponent<CharacterAudio>();
     }
 
     void Start()
@@ -65,6 +67,7 @@ public class Sorceress : MonoBehaviour
         lightningImpact.transform.position = lightningImpactTransform.position;
         Quaternion spellRotation = Quaternion.LookRotation(enemyManager.currentTarget.transform.position - transform.position);
         lightningImpact.transform.rotation = spellRotation;
+        characterAudio.PlaySFX(characterAudio.audioClips[4]);
     }
 
     public void ShootLightning()
@@ -78,6 +81,7 @@ public class Sorceress : MonoBehaviour
         }
 
         lightningImpact.Shoot(shootDir, lightningImpactSpeed);
+        characterAudio.PlaySFX(characterAudio.audioClips[5]);
     }
 
     public void PoisonMist()
