ad4315531dd2573fdeacb8180f23d0e6f9dcccaf
Tirror <74170514+Gyokujin@users.noreply.github.com>
2024-04-23 15:42:37 +0900

    # AudioManager 스크립트 비활성화

--- Assets/Scripts/System/AudioManager.cs
+++ Assets/Scripts/System/AudioManager.cs
@@ -6,59 +6,58 @@ using static PoolManager;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance = null;
-    public enum ActionSFX { Electric }
 
-    [Header("Action")]
-    public AudioClip[] actionClips;
-    [SerializeField]
-    [Range(0, 1)]
-    private float actionVolume;
-    [SerializeField]
-    private int actionCh;
-    private int actionIndex;
-    private AudioSource[] actionAudios;
+    //[Header("Action")]
+    //public AudioClip[] actionClips;
+    //[SerializeField]
+    //[Range(0, 1)]
+    //private float actionVolume;
+    //[SerializeField]
+    //private int actionCh;
+    //private int actionIndex;
+    //private AudioSource[] actionAudios;
 
-    void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+    //void Awake()
+    //{
+    //    if (instance == null)
+    //    {
+    //        instance = this;
+    //    }
+    //    else
+    //    {
+    //        Destroy(gameObject);
+    //    }
 
-        Init();
-    }
+    //    Init();
+    //}
 
-    void Init()
-    {
-        GameObject actionAudio = new GameObject("ActionAudio");
-        actionAudio.transform.parent = transform;
-        actionAudios = new AudioSource[actionCh];
+    //void Init()
+    //{
+    //    GameObject actionAudio = new GameObject("ActionAudio");
+    //    actionAudio.transform.parent = transform;
+    //    actionAudios = new AudioSource[actionCh];
 
-        for (int k = 0; k < actionCh; k++)
-        {
-            actionAudios[k] = actionAudio.AddComponent<AudioSource>();
-            actionAudios[k].playOnAwake = false;
-            actionAudios[k].volume = actionVolume;
-        }
-    }
+    //    for (int k = 0; k < actionCh; k++)
+    //    {
+    //        actionAudios[k] = actionAudio.AddComponent<AudioSource>();
+    //        actionAudios[k].playOnAwake = false;
+    //        actionAudios[k].volume = actionVolume;
+    //    }
+    //}
 
-    public void PlayActionSFX(AudioClip audioClip)
-    {
-        for (int i = 0; i < actionAudios.Length; i++)
-        {
-            int loopIndex = (i + actionIndex) % actionAudios.Length;
+    //public void PlayActionSFX(AudioClip audioClip)
+    //{
+    //    for (int i = 0; i < actionAudios.Length; i++)
+    //    {
+    //        int loopIndex = (i + actionIndex) % actionAudios.Length;
 
-            if (actionAudios[loopIndex].isPlaying)
-                continue;
+    //        if (actionAudios[loopIndex].isPlaying)
+    //            continue;
 
-            actionIndex = loopIndex;
-            actionAudios[loopIndex].clip = audioClip;
-            actionAudios[loopIndex].Play();
-            break;
-        }
-    }
+    //        actionIndex = loopIndex;
+    //        actionAudios[loopIndex].clip = audioClip;
+    //        actionAudios[loopIndex].Play();
+    //        break;
+    //    }
+    //}
 }
