55aa08cf62c88a367ac8983bea14ffbcace819a8
Gyokujin <dd43454548@naver.com>
2024-05-01 03:37:32 +0900

    # GameMessage 스크립트 리팩토링

--- Assets/Scripts/System/GameMessage.cs
+++ Assets/Scripts/System/GameMessage.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 [CreateAssetMenu(menuName = "Game Message/Message")]
 public class GameMessage : ScriptableObject
 {
-    public Text text;
     public string message;
 }
