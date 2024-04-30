da90feed5619716953b9b778995de02e53872de4
Tirror <74170514+Gyokujin@users.noreply.github.com>
2024-04-30 15:56:13 +0900

    # 게임 메시지 기능 초기구현

---
+++ Assets/Scripts/System/GameMessage.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game Message/Message")]
+public class GameMessage : ScriptableObject
+{
+    public string message;
+}
