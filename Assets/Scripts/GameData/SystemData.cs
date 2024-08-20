a2ed8f48c6eb8dce7b00a7e7636d946221e1e638
Gyokujin <dd43454548@naver.com>
2024-08-20 13:52:29 +0900

    # 데이터 수정

--- Assets/Scripts/GameData/SystemData.cs
+++ Assets/Scripts/GameData/SystemData.cs
@@ -239,39 +239,6 @@ namespace SystemData
             readonly get => interactObjTag ?? "Interactable";
             set => interactObjTag = value;
         }
-
-        [Header("Interact Text")]
-        // Interact Item Text
-        private readonly string itemInteractText;
-        public string ItemInteractText
-        {
-            readonly get => itemInteractText ?? "아이템을 획득한다 E";
-            set => ItemInteractText = value;
-        }
-
-        // Interact Message Text
-        private string messageInteractText;
-        public string MessageInteractText
-        {
-            readonly get => messageInteractText ?? "메시지를 확인한다 E";
-            set => messageInteractText = value;
-        }
-
-        // Interact LockDoor Text
-        private string lockDoorInteractText;
-        public string LockDoorInteractText
-        {
-            readonly get => lockDoorInteractText ?? "문을 연다 E";
-            set => lockDoorInteractText = value;
-        }
-
-        // Interact FogWall Text
-        private string fogWallInteractText;
-        public string FogWallInteractText
-        {
-            readonly get => fogWallInteractText ?? "안개 속으로 들어간다 E";
-            set => fogWallInteractText = value;
-        }
     }
 
     public struct InitItemData
--- Assets/Scripts/GameData/UIData.cs
+++ Assets/Scripts/GameData/UIData.cs
@@ -32,4 +32,40 @@ namespace UIData
             set => victoryDelay = value;
         }
     }
+
+    public struct MessageUIData
+    {
+        [Header("Interact Text")]
+        // Interact Item Text
+        private readonly string itemInteractText;
+        public string ItemInteractText
+        {
+            readonly get => itemInteractText ?? "아이템을 획득한다 E";
+            set => ItemInteractText = value;
+        }
+
+        // Interact Message Text
+        private string messageInteractText;
+        public string MessageInteractText
+        {
+            readonly get => messageInteractText ?? "메시지를 확인한다 E";
+            set => messageInteractText = value;
+        }
+
+        // Interact LockDoor Text
+        private string lockDoorInteractText;
+        public string LockDoorInteractText
+        {
+            readonly get => lockDoorInteractText ?? "문을 연다 E";
+            set => lockDoorInteractText = value;
+        }
+
+        // Interact FogWall Text
+        private string fogWallInteractText;
+        public string FogWallInteractText
+        {
+            readonly get => fogWallInteractText ?? "안개 속으로 들어간다 E";
+            set => fogWallInteractText = value;
+        }
+    }
 }
