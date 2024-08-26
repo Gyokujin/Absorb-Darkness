da27bd14d869c352f343072d7337d31fdbe76159
Gyokujin <dd43454548@naver.com>
2024-08-27 03:42:54 +0900

    # 유니티 VFX 리소스 파일 정리

---
+++ Assets/Scripts/LightFade.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFade : MonoBehaviour {
+
+    [SerializeField]
+    private float fadeTime = 0f;
+    private float t = 0f;
+    private new Light light;
+	
+	void Awake()
+	{
+		Init();
+	}
+	
+	void Init()
+	{
+		light = GetComponent<Light>();
+	}
+
+	void Start()
+	{
+		StartCoroutine(Fade());
+	}
+	
+	IEnumerator Fade()
+	{
+		t = 0.0f;
+		
+		while (t < fadeTime)
+		{
+			t += Time.deltaTime;
+			light.intensity = Mathf.Lerp(1f, 0f, t / fadeTime);
+			yield return null;
+		}
+	}
+}
