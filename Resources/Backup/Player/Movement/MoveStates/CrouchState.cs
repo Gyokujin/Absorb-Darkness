c3740e401427dd2fa1cd7caf2e65c5075f85853d
Gyokujin <dd43454548@naver.com>
2024-12-30 14:08:54 +0900

    # 리소스 파일 정리

---
+++ Resources/Backup/Player/Movement/MoveStates/CrouchState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrouchState : MovementBaseState
+{
+    public override void EnterState(MovementStateManager state)
+    {
+
+    }
+
+    public override void UpdateState(MovementStateManager state)
+    {
+
+    }
+}
---
+++ Resources/Backup/Player/Movement/MovementBaseState.cs
@@ -0,0 +1,5 @@
+public abstract class MovementBaseState
+{
+    public abstract void EnterState(MovementStateManager state);
+    public abstract void UpdateState(MovementStateManager state);
+}
